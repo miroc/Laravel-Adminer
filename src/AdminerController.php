26210ebeb08b2df55efe3787d97e2723cfc14b71
Miroslav Svitok <mirosvitok@gmail.com>
2016-12-11 00:04:34 +0100

    Update to Adminer 4.2.5

--- src/AdminerController.php
+++ src/AdminerController.php
@@ -6,7 +6,7 @@ class AdminerController extends Controller {
 
     public function index()
     {
-        require('adminer-4.2.4-en.php');
+        require('adminer-4.2.5-en.php');
         return new EmptyResponse();
     }
 
