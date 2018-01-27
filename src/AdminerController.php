a0b395b3b6640ed08639c775134cf9d7eef11b9c
repat <repat@repat.de>
2018-01-26 18:06:46 -0800

    update to 4.5.0

--- src/AdminerController.php
+++ src/AdminerController.php
@@ -4,10 +4,9 @@ use Illuminate\Routing\Controller;
 
 class AdminerController extends Controller {
 
-    public function index()
-    {
-        require('adminer-4.3.1-en.php');
-        return new EmptyResponse();
-    }
+	public function index() {
+		require 'adminer-4.5.0-en.php';
+		return new EmptyResponse();
+	}
 
 }
